e1ff6120656ec301cee99a0cfc81ea76f0cca450
YuSheng <chester@cwyark.me>
2025-05-13 10:02:02 +0800

    update code

--- os/thread_pass_argument.cpp
+++ os/thread_pass_argument.cpp
@@ -1,16 +1,13 @@
 #include <iostream>
-#include <mutex>
 #include <thread>
-
-using namespace std;
 
 void threadFunc(int n, int *N) { *N = n + 1; }
 
 int main() {
   int n = 10;
   int N = 0;
-  thread thread_1(threadFunc, n, &N);
+  std::thread thread_1(threadFunc, n, &N);
   thread_1.join();
-  cout << N << endl;
-  exit(EXIT_SUCCESS);
+  std::cout << N << std::endl;
+  return 0;
 }
